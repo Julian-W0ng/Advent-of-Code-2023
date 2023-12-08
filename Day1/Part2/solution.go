fc8a7784e5de31da33053592c3cec6d03442839a
Julian <julian.connor.wong@gmail.com>
2023-12-07 21:39:52 -0500

    Day1 Done

--- Day1/Part2/solution.go
+++ Day1/Part2/solution.go
@@ -20,45 +20,58 @@ func main() {
 	var sum int = 0
 	// array of digits as characters
 	charDigits := [10]byte{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}
-	// constant array of digits as words
-	// digitsWords := [10]string{"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+	// array of digits as words
+	wordDigits := [10]string{"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
 
 	for _, line := range lines {
 		firstDigit := -1
-		// firstDigitIndex := len(line)
+		firstIndex := len(line)
 		lastDigit := -1
-		// lastDigitIndex := 0
-
-		found := false
+		lastIndex := 0
 
 		// get the first digit of the line
-		for i := 0; i < len(line) && !found; i++ {
+		for i := 0; i < len(line); i++ {
 			for j, d := range charDigits {
 				if d == line[i] {
 					firstDigit = j
-					// firstDigitIndex = i
-					found = true
-					break
+					firstIndex = i
+					goto foundFirst
 				}
 			}
 		}
 
-		found = false
+	foundFirst:
+
+		for i, d := range wordDigits {
+			var index int = strings.Index(line, d)
+			if index != -1 && index < firstIndex {
+				firstDigit = i
+				firstIndex = index
+			}
+		}
 
 		// get the last digit of the line
-		for i := len(line) - 1; i >= 0 && !found; i-- {
+		for i := len(line) - 1; i >= 0; i-- {
 			for j, d := range charDigits {
 				if d == line[i] {
 					lastDigit = j
-					// firstDigitIndex = i
-					found = true
-					break
+					lastIndex = i
+					goto foundLast
 				}
+			}
+		}
+
+	foundLast:
+
+		for i, d := range wordDigits {
+			var index int = strings.LastIndex(line, d)
+			if index != -1 && index > lastIndex {
+				lastDigit = i
+				lastIndex = index
 			}
 		}
 
 		sum += firstDigit*10 + lastDigit
-
 	}
 
 	fmt.Println("Sum of all numbers in the file is: ", sum)
